7b2a1740313608e002744b6e1d5629fedd4d86b5
hao wang <ge_nah@hotmail.com>
2019-04-23 00:45:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sent)
-  array = sent.split
-  new_arr = []
-  array.each { |word| new_arr << word.reverse}
-  new_arr.join(" ")
-end
-
 # def reverse_each_word(sent)
 #   array = sent.split
-#   return array.collect { |word| word.reverse}.join(" ")
+#   new_arr = []
+#   array.each { |word| new_arr << word.reverse}
+#   new_arr.join(" ")
 # end
+
+def reverse_each_word(sent)
+  array = sent.split
+  return array.collect { |word| word.reverse}.join(" ")
+end
